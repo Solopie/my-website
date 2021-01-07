b75bc0ca8ebecb92d8f5aeb90d67aebdbe8c4d0a
Jesse Nguyen <jesse_nguyen9@yahoo.com>
2021-01-07 13:01:32 +1100

    Clickable images

--- server.js
+++ server.js
@@ -2,11 +2,22 @@ const express = require("express");
 const serveIndex = require("serve-index");
 const app = express();
 app.set("view engine", "pug");
+
+// No indexing of my website
+app.use((req, res, next) => {
+    res.setHeader("X-Robots-Tag", "none");
+    next();
+});
+
 app.use(express.static(__dirname + "/public"));
 app.use("/images", serveIndex("public/images", { "icons": true }));
 
 app.get("/", (req, res) => {
     res.render("index", { title: "Homepage" });
+});
+
+app.get("/projects", (req, res) => {
+    res.send("Projects page in progress...");
 });
 
 const server = app.listen(3000, () => {
